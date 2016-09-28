38155af42b6b23196e057d96481be800c573d465
Matthew Bae <mbae@hmc.edu>
2016-09-28 14:44:21 -0700

    did internal lab

--- src/main/scala/internal/ForLoop.scala
+++ src/main/scala/internal/ForLoop.scala
@@ -12,7 +12,13 @@ package internal
 object ForLoop extends App {
 
   // define the new control-flow structure here 
-  
+  def for_loop(init: => Unit)(cond: => Boolean)(inc: => Unit)(body: => Unit) {
+  	init
+  	while (cond) {
+  		body
+  		inc
+  	}
+  }
   var i = 0;
   for_loop(i = 0)(i < 10)(i += 2) {
     println(i);
